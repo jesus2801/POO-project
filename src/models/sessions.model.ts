1ad5120a5e5eeb47a3c025f3b8ede100c0f35f71
jesus2801 <jesusdavid280147@gmail.com>
2023-04-05 15:56:09 -0500

    files utils updated so we can easily manage csv operations, also sessions model updated. code organization and commenting is missing

--- src/models/sessions.model.ts
+++ src/models/sessions.model.ts
@@ -1,30 +1,77 @@
 import { db_path } from "../config/db.config";
 import { Session } from "../interfaces/db.interface";
 import fileUtils from "../utils/file.utils";
+import { v4 } from "uuid";
 
 class SessionModel {
-    file_name: string;
-    file_path: string;
+  file_name: string;
+  file_path: string;
+  headers: string[];
 
-    //create the file if it doesn't exist
-    constructor() {
-        this.file_name = "sessions.csv";
-        this.file_path = db_path;
-        fileUtils.createEmptyFile(this.file_name, this.file_path)
-    }
+  //create the file if it doesn't exist
+  constructor() {
+    this.file_name = "sessions.csv";
+    this.file_path = db_path;
+    this.headers = [
+      "id",
+      "userId",
+      "duration",
+      "reviewed_cards",
+      "reached_goals",
+      "initDate",
+      "endDate",
+    ];
+    this.startFile();
+  }
 
-    //USE THROW FOR ERROR HANDLING
+  private startFile(overwrite: boolean = false){
+    fileUtils.createEmptyFile(this.file_name, this.file_path, this.headers, overwrite);
+  }
 
-    //this functions returns all sessions from a user id
-    //@ts-ignore
-    public async getUserSessions(userId: number): Promise<Session[]> {}
+  //USE THROW FOR ERROR HANDLING
 
-    //this function creates a session and returns it with the new setted values
-    //@ts-ignore
-    public async createSession(session: Omit<Session, "id">): Promise<Session> {}
+  //this functions returns all sessions from a user id
+  //@ts-ignore
+  public async getUserSessions(userId: string): Promise<Session[]> {
+    const sessions: Session[] = await fileUtils.filter(
+      this.file_path + this.file_name,
+      { userId },
+      false
+    );
+    return sessions;
+  }
 
-    //this function deletes a session
-    public async deleteSession(id: number): Promise<void> {}
+  //this function creates a session and returns it with the new setted values
+  //@ts-ignore
+  public async createSession(session: Omit<Session, "id">): Promise<Session> {
+    const newSession: Session = { ...session, id: v4() };
+    await fileUtils.append(
+      [newSession],
+      this.file_path + this.file_name,
+      this.headers
+    );
+    return newSession;
+  }
+
+  //this function deletes a session
+  public async deleteSession(id: string): Promise<void> {
+    const data = await fileUtils.filter(
+      this.file_path + this.file_name,
+      { id },
+      true
+    );
+
+    console.log(data)
+
+    if (data.length > 0)
+      await fileUtils.create(
+        this.file_path + this.file_name,
+        data,
+        this.headers
+      );
+    else
+        this.startFile(true);
+  }
 }
 
 export default new SessionModel();
--- src/utils/file.utils.ts
+++ src/utils/file.utils.ts
@@ -1,25 +1,117 @@
-import { existsSync, writeFileSync } from "fs"
+import { existsSync, writeFileSync, createWriteStream } from "fs";
 import { join } from "path";
 import logger from "../config/logger";
-
+import { FormatterOptionsArgs, Row, writeToStream } from "@fast-csv/format";
+import { csvOptions } from "../config/db.config";
+import { parse } from "fast-csv";
+import { createReadStream } from "fs";
 
 class FileUtils {
-    //create a file
-    public async createEmptyFile(file_name: string, file_path: string) {
-        //get the file path
-        const path = join(file_path, file_name)
+  private readonly writeOpts: FormatterOptionsArgs<Row, Row>;
 
-        //check if the file exists
-        if (!existsSync(path)) {
-            //create the file
-            try {
-                writeFileSync(path, '');
-                logger(`File ${file_name} created at ${file_path}`);
-            } catch {
-                logger(`There was an error creating ${file_name} at ${file_path}`)
-            }
-        }
+  constructor() {
+    this.writeOpts = { includeEndRowDelimiter: true, delimiter: ";" };
+  }
+
+  //create a file
+  public async createEmptyFile(
+    file_name: string,
+    file_path: string,
+    headers: string[],
+    overwrite: boolean
+  ) {
+    //get the file path
+    const path = join(file_path, file_name);
+
+    //check if the file exists
+    if (!existsSync(path) || (existsSync(path) && overwrite)) {
+      //create the file
+      try {
+        writeFileSync(path, headers.join(";") + "\n");
+        logger(`File ${file_name} created at ${file_path}`);
+      } catch {
+        logger(`There was an error creating ${file_name} at ${file_path}`);
+      }
     }
+  }
+
+  private write(
+    stream: NodeJS.WritableStream,
+    rows: Row[],
+    options: FormatterOptionsArgs<Row, Row>
+  ): Promise<void> {
+    return new Promise((res, rej) => {
+      writeToStream(stream, rows, options)
+        .on("error", (err: Error) => rej(err))
+        .on("finish", () => res());
+    });
+  }
+
+  public create(path: string, rows: Row[], headers: string[]): Promise<void> {
+    return this.write(createWriteStream(path), rows, {
+      ...this.writeOpts,
+      headers,
+    });
+  }
+
+  public append(rows: Row[], path: string, headers: string[]): Promise<void> {
+    return this.write(createWriteStream(path, { flags: "a" }), rows, {
+      ...this.writeOpts,
+      // dont write the headers when appending
+      writeHeaders: false,
+      headers,
+    } as FormatterOptionsArgs<Row, Row>);
+  }
+
+  public filter(path: string, filters: any, inverse: boolean): Promise<any[]> {
+    const rows: any[] = [];
+
+    return new Promise((res, rej) => {
+      createReadStream(path)
+        .pipe(parse(csvOptions))
+        .on("error", (err) => {
+          rej(new Error("unexpected error"));
+        })
+        .on("data", (data) => {
+          let correct = true;
+          for (let filter in filters)
+            if (data[filter] != filters[filter]) correct = false;
+
+          if ((correct && !inverse) || (!correct && inverse)) rows.push(data);
+        })
+        .on("end", () => {
+          res(rows);
+        });
+    });
+  }
+
+  public update(
+    path: string,
+    filter: string,
+    value: string,
+    newRow: any
+  ): Promise<any[]> {
+    const rows: any[] = [];
+
+    return new Promise((res, rej) => {
+      createReadStream(path)
+        .pipe(parse(csvOptions))
+        .on("error", (err) => {
+          rej(new Error("unexpected error"));
+        })
+        .on("data", (data) => {
+          if (data[filter] == value) {
+            rows.push(newRow);
+          } else {
+            rows.push(data);
+          }
+
+        })
+        .on("end", () => {
+          res(rows);
+        });
+    });
+  }
 }
 
 export default new FileUtils();
